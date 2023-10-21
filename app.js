ccfa810a2e6a3cc44b429cd12bfc4adbcaa92df8
n0vay <koshantanu@gmail.com>
2023-10-20 23:10:30 -0400

    added mongoose created schema

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
-// connect to mongodb
+// connect to mongodb & listen for requests
 const dbURI =
-  "mongodb+srv://koshantanu:Shan@123@blogdb.peymb5y.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://koshantanu:shan123@blogdb.peymb5y.mongodb.net/blogdb?retryWrites=true&w=majority";
 
-// listen for requests
-app.listen(3000);
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs");
 
 // middleware & static files
 app.use(express.static("public"));
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// list all blogs data
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// find a single blog
+app.get("/single-blog", (req, res) => {
+  Blog.findById("652f373f39398a79db7e3112")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use((req, res, next) => {
   console.log("new request made:");
